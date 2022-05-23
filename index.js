f0e456fc5fdda3d23dea3fc7ede70e47cd9fac23
estatelelashvili <35972253+estatelelashvili@users.noreply.github.com>
2022-05-23 08:26:54 +0400

    feat: update html and js files

--- index.js
+++ index.js
@@ -57,74 +57,203 @@ function appendData(data) {
 
   let grandCartCont = document.createElement("div");
   grandCartCont.className = "grand-cart-container";
+
   let cartContainer = document.createElement("div");
-  cartContainer.style.cssText = `position:absolute; top: 50%; left: 50%; width: 100px`;
-  cartContainer.innerHTML = "mother of cart";
+  cartContainer.className = "cart-container";
+
+  //Cart show/hide toggle button
+  let btnToggleCart = document.createElement("button");
+  let btnOrder = document.createElement("button");
+  let itemCount = 0;
+  btnToggleCart.textContent = `Show Cart of ${itemCount} items`;
+  btnOrder.textContent = "Confirm order";
+  btnToggleCart.style.cssText = `position: absolute; top: 25%; left: 0; width: 100px`;
+  // btnOrder.style.cssText = `position: absolute; top: 25%; left: 0; width: 100px`;
+  btnOrder.style.cssText = `position:absolute;  width: 115px; left: 15%;
+  bottom: 10px;`;
+
   let cart = document.createElement("div");
+  let babyCart = document.createElement("div");
 
-  let testInputName = document.createElement("p");
-  // cartContainer.appendChild(testInputName);
-  // let inputName = document.createElement("input");
-  // inputName.type = "text";
-  // inputName.value = "type here";
-  // cartContainer.appendChild(inputName);
-  // cart.appendChild(inputName);
-  testInputName.innerText = "test";
-  cartContainer.appendChild(testInputName);
-  // testInputName.innerText = inputName.value;
-  let total = document.createElement("p");
-  total.innerText = "0";
-  cartContainer.appendChild(total);
-  let titleAll = document.createElement("div");
-  titleAll.innerText = "All titles";
-  cartContainer.appendChild(titleAll);
+  //Total price p tag
+  let totalPrice = document.createElement("p");
+  let hrCart = document.createElement("hr");
+  let unorderedList = document.createElement("ul");
+  let listItem1 = document.createElement("li");
+  let listItem2 = document.createElement("li");
+  let listItem3 = document.createElement("li");
+  listItem1.textContent = "Image";
+  listItem2.textContent = "Name";
+  listItem3.textContent = "Price";
+  unorderedList.appendChild(listItem1);
+  unorderedList.appendChild(listItem2);
+  unorderedList.appendChild(listItem3);
 
-  var form = document.createElement("form");
-  // // document.body.appendChild(form);
-  // // form.method = "POST";
-  // // form.action = "login.php";
-  var element1 = document.createElement("input");
-  // element1.name = "un";
-  element1.value = "un";
-  // element1.type = "hidden";
-  form.appendChild(element1);
-  var element2 = document.createElement("input");
-  // element2.name = "pw";
-  element2.value = "pw";
-  var s = document.createElement("input");
-  s.setAttribute("type", "submit");
-  s.setAttribute("value", "Submit");
-  s.onclick = function () {
-    testInputName.innerText = `${element1.value} && ${element2.value}`;
+  let clearAll = document.createElement("button");
+  clearAll.innerText = "Clear All";
+  clearAll.onclick = () => {
+    testTable.innerHTML = "";
+    itemCount = 0;
+    specialPriceTag = 0;
+    totalPrice.textContent = `Total: ${specialPriceTag}$`;
+    btnToggleCart.textContent = `Show Cart of ${itemCount} items`;
   };
-  // element2.type = "hidden";
-  form.appendChild(element2);
-  form.appendChild(s);
-  // form.submit();
-  cartContainer.appendChild(form);
 
-  // let total = document.createElement("p");
-  // total.innerText = "0";
+  let specialPriceTag = 0;
+  let val = 0;
+  totalPrice.innerText = `Total: ${specialPriceTag}$`;
+  let total = document.createElement("p");
+  cart.appendChild(totalPrice);
+  cart.appendChild(hrCart);
+  cart.appendChild(unorderedList);
 
   cart.className = "cart-content";
-  cart.style.cssText = `background-color: red; position:relative; 
-  top: 50%; left: 50%; height: 500px; width: 500px; display: none;`;
-  // cartContainer.appendChild(total);
-  cartContainer.onclick = function () {
+  babyCart.className = "baby-cart-content";
+  let agent_1;
+
+  babyCart.setAttribute("id", agent_1);
+
+  cart.style.cssText = `display: none;`;
+  clearAll.style.cssText = `position:absolute;  width: 75px; left: 55%;
+  bottom: 10px;`;
+  cart.appendChild(clearAll);
+  cart.appendChild(btnOrder);
+
+  //Table
+
+  let testTable = document.createElement("table");
+
+  //TOGGLE CART BUTTON
+
+  btnToggleCart.onclick = function () {
     if (cart.style.display === "block") {
       cart.style.display = "none";
     } else {
       cart.style.display = "block";
     }
   };
+
+  //REDIRECT TO ORDER PAGE
+
+  btnOrder.onclick = function () {
+    location.href = "OrderForm.html";
+  };
+
+  //Cart append elements
+  cart.appendChild(babyCart);
   cartContainer.appendChild(cart);
+
   grandCartCont.appendChild(cartContainer);
+
+  grandCartCont.appendChild(btnToggleCart);
+
   header.append(grandCartCont);
+
+  let detailedInformationContainer = document.createElement("div");
+  detailedInformationContainer.className = "detailed-information-container";
+  let disableShowMore = true;
+
+  // IMPLEMENT DRAG AND DROP
+  cartContainer.addEventListener("dragover", allowDrop);
+  // grandCartCont.addEventListener("dragover", allowDrop);
+  function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  // babyCart.addEventListener("dragover", allowDrop);
+  // // grandCartCont.addEventListener("dragover", allowDrop);
+  // function allowDrop(ev) {
+  //   ev.preventDefault();
+  // }
+
+  cartContainer.addEventListener("drop", drop);
+  // babyCart.addEventListener("drop", drop);
+  // grandCartCont.addEventListener("drop", drop);
+
+  function drop(ev) {
+    let title2 = document.createElement("div");
+    let author2 = document.createElement("p");
+    let bookName2 = document.createElement("p");
+    let image2 = document.createElement("img");
+    let price2 = document.createElement("p");
+    let btnX2 = document.createElement("button");
+
+    price2.className = "elm-p";
+    bookName2.className = "elm-t";
+    author2.className = "elm-a";
+    image2.className = "elm-img";
+    image2.src = data[+agent_1].imageLink;
+
+    author2.innerHTML = data[+agent_1].author;
+    price2.innerHTML = data[+agent_1].price + "$";
+    bookName2.innerHTML = data[+agent_1].title;
+    btnX2.innerText = "X";
+
+    if (cart.textContent.includes(price2.textContent)) {
+    } else {
+      let middleRow2 = document.createElement("tr");
+      middleRow2.style.cssText = `display: flex; align-items: center;`;
+
+      let titleTD2 = document.createElement("td");
+      let imageTD2 = document.createElement("td");
+      let priceTD2 = document.createElement("td");
+      let btnXTD2 = document.createElement("td");
+
+      imageTD2.appendChild(image2);
+      btnXTD2.appendChild(btnX2);
+      title2.appendChild(bookName2);
+      title2.appendChild(author2);
+      titleTD2.appendChild(title2);
+      priceTD2.appendChild(price2);
+
+      middleRow2.appendChild(imageTD2);
+      middleRow2.appendChild(titleTD2);
+      middleRow2.appendChild(priceTD2);
+      middleRow2.appendChild(btnXTD2);
+
+      testTable.appendChild(middleRow2);
+
+      let empArr = [];
+      const collection = document.getElementsByClassName("elm-p");
+      itemCount = collection.length;
+      btnToggleCart.textContent = `Show Cart of ${itemCount} items`;
+
+      for (let i = 0; i < collection.length; i++) {
+        empArr.push(collection[i].textContent.replace("$", ""));
+      }
+
+      let sumArr = empArr.map(Number);
+
+      total.textContent = sumArr.reduce((p, c) => p + c);
+
+      specialPriceTag = sumArr.reduce((p, c) => p + c);
+      totalPrice.textContent = `Total: ${specialPriceTag}$`;
+
+      btnX2.onclick = () => {
+        val = parseInt(total.textContent) - parseInt(price2.textContent);
+        total.textContent = val.toString();
+        totalPrice.textContent = `Total: ${
+          specialPriceTag - parseInt(price2.textContent)
+        }`;
+        totalPrice.textContent = `Total: ${val.toString()}$`;
+
+        middleRow2.remove();
+        const collection = document.getElementsByClassName("elm-p");
+        itemCount = collection.length;
+        btnToggleCart.textContent = `Show Cart of ${itemCount} items`;
+      };
+    }
+
+    ev.preventDefault();
+  }
+
+  //BIG LOOP STARTS HERE
 
   for (var i = 0; i < data.length; i++) {
     const wrapper = document.createElement("div");
     const info = document.createElement("div");
+
     const card = document.createElement("div");
+
     const overlay = document.createElement("div");
     const text = document.createElement("div");
     const image = document.createElement("img");
@@ -137,37 +266,123 @@ function appendData(data) {
     const slideUpAuthor = document.createElement("p");
     const btnAddToCart = document.createElement("button");
 
+    let imagSrc = data[i].imageLink;
     slideUpPrice.className = "slide-up-price";
     slideUpTitle.className = "slide-up-title";
     slideUpAuthor.className = "slide-up-author";
     btnAddToCart.className = "btn-add-to-cart";
     slideUpPrice.innerHTML = data[i].price + "$";
     slideUpTitle.innerHTML = data[i].title;
-    slideUpAuthor.innerHTML = "Author: " + data[i].author;
+    slideUpAuthor.innerHTML = data[i].author;
     btnAddToCart.innerHTML = "add to cart";
 
-    btnAddToCart.addEventListener("click", addBookToBasket);
+    image.setAttribute("id", `${i}`);
 
+    let detailedInformation = document.createElement("div");
+    let btnInfoX = document.createElement("button");
+    detailedInformation.className = "detailed-information";
+    btnInfoX.className = "btn-info-x";
+    btnInfoX.textContent = "X";
+
+    btnInfoX.addEventListener("click", hideDetails);
+    function hideDetails() {
+      disableShowMore = true;
+      detailedInformation.style.display = "none";
+    }
+    const descriptionPopUp = document.createElement("p");
+    const disPhoto = document.createElement("img");
+    disPhoto.src = data[i].imageLink;
+    descriptionPopUp.innerText = data[i].description;
+    detailedInformation.appendChild(btnInfoX);
+    detailedInformation.appendChild(disPhoto);
+    detailedInformation.appendChild(descriptionPopUp);
+    detailedInformationContainer.appendChild(detailedInformation);
+    fragment.appendChild(detailedInformationContainer);
+    /////////////////////////////////////
+
+    const btnShowMore = document.createElement("button");
+    btnShowMore.className = "btn-show-more";
+    btnShowMore.innerHTML = "Show more";
+    btnShowMore.addEventListener("click", showDetails);
+    function showDetails() {
+      if (disableShowMore) {
+        disableShowMore = false;
+        detailedInformation.style.display = "block";
+      }
+    }
+    babyCart.append(testTable);
+    btnAddToCart.addEventListener("click", addBookToBasket);
     function addBookToBasket() {
-      let elm = document.createElement("div");
-      elm.className = "all-elms";
+      let elmInfo = document.createElement("div");
+      let elmP = document.createElement("div");
+      let elmT = document.createElement("div");
+      let elmA = document.createElement("div");
+      let elmImage = document.createElement("img");
+      elmInfo.className = "elm-info";
+      elmP.className = "elm-p";
+      elmT.className = "elm-t";
+      elmA.className = "elm-a";
+      elmImage.className = "elm-img";
       let btnX = document.createElement("button");
 
-      let btnConfirmOrder = document.createElement("button");
+      btnX.className = "btn-x";
       btnX.innerHTML = "X";
-      elm.innerHTML = slideUpPrice.textContent;
+      elmP.innerHTML = slideUpPrice.textContent;
+      elmT.innerHTML = slideUpTitle.textContent;
+      elmA.innerHTML = slideUpAuthor.textContent;
+      elmImage.src = imagSrc;
+      elmImage.style.cssText = `background-color: red;`;
       if (cart.textContent.includes(slideUpPrice.textContent)) {
       } else {
-        cart.append(elm);
-        cart.append(btnX);
-        const numbers = cart.textContent.split("$X").map(Number);
-        total.textContent = numbers.reduce((p, c) => p + c);
+        let middleRow = document.createElement("tr");
+        middleRow.style.cssText = `display: flex; align-items: center;`;
+
+        let titleTD = document.createElement("td");
+        let btnXTD = document.createElement("td");
+        let imageTD = document.createElement("td");
+        let priceTD = document.createElement("td");
+
+        imageTD.style.cssText = `align-self: stretch;`;
+        imageTD.appendChild(elmImage);
+        priceTD.appendChild(elmP);
+        btnXTD.appendChild(btnX);
+        titleTD.appendChild(elmT);
+        titleTD.appendChild(elmA);
+        middleRow.appendChild(imageTD);
+        middleRow.appendChild(titleTD);
+        middleRow.appendChild(priceTD);
+        middleRow.appendChild(btnXTD);
+        testTable.appendChild(middleRow);
+
+        let empArr = [];
+        const collection = document.getElementsByClassName("elm-p");
+        itemCount = collection.length;
+        btnToggleCart.textContent = `Show Cart of ${itemCount} items`;
+
+        for (let i = 0; i < collection.length; i++) {
+          empArr.push(collection[i].textContent.replace("$", ""));
+        }
+
+        let sumArr = empArr.map(Number);
+
+        total.textContent = sumArr.reduce((p, c) => p + c);
+
+        specialPriceTag = sumArr.reduce((p, c) => p + c);
+        totalPrice.textContent = `Total: ${specialPriceTag}$`;
+        console.log(typeof specialPriceTag);
         titleAll.textContent += slideUpTitle.textContent;
         btnX.onclick = function () {
-          let val = parseInt(total.textContent) - parseInt(elm.textContent);
+          val = parseInt(total.textContent) - parseInt(elmP.textContent);
           total.textContent = val.toString();
-          elm.remove();
-          btnX.remove();
+          totalPrice.textContent = `Total: ${
+            specialPriceTag - parseInt(elmP.textContent)
+          }`;
+          totalPrice.textContent = `Total: ${val.toString()}$`;
+
+          middleRow.remove();
+          const collection = document.getElementsByClassName("elm-p");
+          itemCount = collection.length;
+          btnToggleCart.textContent = `Show Cart of ${itemCount} items`;
         };
       }
     }
@@ -175,6 +390,7 @@ function appendData(data) {
     wrapper.className = "wrapper";
     info.className = "info";
     card.className = "card";
+
     overlay.className = "overlay";
     text.className = "text";
     title.className = "title";
@@ -193,6 +409,7 @@ function appendData(data) {
     text.appendChild(slideUpTitle);
     text.appendChild(slideUpAuthor);
     text.appendChild(btnAddToCart);
+    text.appendChild(btnShowMore);
 
     overlay.appendChild(text);
 
@@ -202,7 +419,13 @@ function appendData(data) {
     wrapper.appendChild(card);
     container.appendChild(wrapper);
   }
-
+  document.body.addEventListener("dragstart", drag);
+  function drag(ev) {
+    // grandCartCont.innerText = "drap here";
+    // btnToggleCart.innerText = "drap here";
+    agent_1 = ev.target.id;
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
   fragment.append(container);
   document.body.appendChild(fragment);
 }
